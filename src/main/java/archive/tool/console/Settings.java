25cf5d3867dd55817f77bedbfaf15c8dede72182
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-02-27 09:54:10 +0800

    Settings class added to hold compression/decompression settings.

---
+++ src/main/java/archive/tool/console/Settings.java
@@ -0,0 +1,15 @@
+package archive.tool.console;
+
+public class Settings {
+
+    public static Action action;
+
+    //compress settings
+    public static String inputZipDir;
+    public static String outputZipDir;
+    public static long maxSize;
+
+    //decompress settings
+    public static String inputUnzipDir;
+    public static String outputUnzipDir;
+}
