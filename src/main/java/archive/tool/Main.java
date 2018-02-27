cefbb6a21b87f2ce4a4a75476b94a9b403e0a519
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-02-27 09:59:09 +0800

    Main class added to run the program.

---
+++ src/main/java/archive/tool/Main.java
@@ -0,0 +1,13 @@
+package archive.tool;
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+        System.out.println("Start program");
+
+        // TODO: Do the work here.
+
+        System.out.println("End Program");
+
+    }
+}
