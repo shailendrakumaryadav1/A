749ca961d46c2d385bea8c1089b97ae98f914c35
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-02-27 12:55:24 +0800

    Compressor and Decompressor Interfaces added

---
+++ src/main/java/archive/tool/core/Compressor.java
@@ -0,0 +1,9 @@
+package archive.tool.core;
+
+import java.io.IOException;
+
+public interface Compressor {
+
+    void compress() throws IOException;
+
+}
---
+++ src/main/java/archive/tool/core/Decompressor.java
@@ -0,0 +1,9 @@
+package archive.tool.core;
+
+import java.io.IOException;
+
+public interface Decompressor {
+
+    void decompress() throws IOException;
+
+}
