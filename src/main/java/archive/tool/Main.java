393f128716a729298cdaae20e37779d72c6dfdcc
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-02-27 10:44:39 +0800

    Console class created to hold User inputs as Settings.

--- src/main/java/archive/tool/Main.java
+++ src/main/java/archive/tool/Main.java
@@ -1,11 +1,23 @@
 package archive.tool;
 
+import archive.tool.console.Action;
+import archive.tool.console.Console;
+import archive.tool.console.Settings;
+
 public class Main {
 
     public static void main(String... args) throws Exception {
         System.out.println("Start program");
 
         // TODO: Do the work here.
+        Console console = new Console();
+        console.enterSettings();
+
+        if (Settings.action == Action.COMPRESS) {
+            // TODO: Do compress here using settings
+        } else {
+            // TODO: Do decompress here using settings
+        }
 
         System.out.println("End Program");
 
---
+++ src/main/java/archive/tool/console/Console.java
@@ -0,0 +1,57 @@
+package archive.tool.console;
+
+import java.util.Scanner;
+
+public class Console {
+
+    public void enterSettings() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n Select an action(enter 0 or 1):");
+        System.out.println("[0] Compress");
+        System.out.println("[1] Decompress");
+
+        int action = scanner.nextInt();
+        switch (action) {
+            case 0:
+                Settings.action = Action.COMPRESS;
+                enterCompressSettings(scanner);
+                break;
+            case 1:
+                Settings.action = Action.DECOMPRESS;
+                enterDecompressSettings(scanner);
+                break;
+            default:
+                System.out.println("Incorrect action, enter 0 or 1");
+                System.exit(0);
+
+        }
+    }
+
+    private void enterCompressSettings(Scanner scanner) {
+
+        System.out.println("\n Enter path to Input directory:");
+        String inputDir = scanner.next();
+        Settings.inputZipDir = inputDir;
+
+        System.out.println("\n Enter path to Output directory:");
+        String outputDir = scanner.next();
+        Settings.outputZipDir = outputDir;
+
+        System.out.println("\n Enter max output file size in bytes:");
+        Long maxSize = scanner.nextLong();
+        Settings.maxSize = maxSize;
+    }
+
+    private void enterDecompressSettings(Scanner scanner) {
+
+        System.out.println("\n Enter path to Input directory:");
+        String inputDir = scanner.next();
+        Settings.inputUnzipDir = inputDir;
+
+        System.out.println("\n Enter path to Output directory:");
+        String outputDir = scanner.next();
+        Settings.outputUnzipDir = outputDir;
+    }
+}
