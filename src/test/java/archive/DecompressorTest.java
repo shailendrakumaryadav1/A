5e2194135000497ef59fcbc9bda5ace681ffa0f2
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-03-01 17:29:06 +0800

    Test cases for Decompression added.

--- src/test/java/archive/DecompressorTest.java
+++ src/test/java/archive/DecompressorTest.java
@@ -1,9 +1,13 @@
 package archive;
 
 import archive.tool.console.Settings;
-import archive.tool.core.interfaces.Decompressor;
 import archive.tool.core.impl.DecompressorImpl;
+import archive.tool.core.interfaces.Decompressor;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class DecompressorTest {
 
@@ -15,7 +19,32 @@ public class DecompressorTest {
         Settings.outputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/in_1";
 
         Decompressor decompressor = new DecompressorImpl();
-        decompressor.decompress();
+        Assert.assertTrue(decompressor.decompress());
         System.out.println("Done");
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldCompressFailsForSourceDirDoesNotExist() throws IOException {
+
+        Settings.inputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out" + Math.random() + Math.random() + File.separator;
+        System.out.println(Settings.inputUnzipDir);
+        Settings.outputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/in";
+
+        Decompressor decompressor = new DecompressorImpl();
+        decompressor.decompress();
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldCompressFailsForSourceIsNotADirectory() throws IOException {
+
+        Settings.inputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out/staff.csv";
+        System.out.println(Settings.inputUnzipDir);
+        Settings.outputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/in";
+
+        Decompressor decompressor = new DecompressorImpl();
+        decompressor.decompress();
+
+    }
+
 }
