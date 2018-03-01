42d6d6395ddd8690757abbeeeaa31b11e754fca6
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-03-01 17:31:12 +0800

    Print statements removed from test cases.

--- src/test/java/archive/CompressorTest.java
+++ src/test/java/archive/CompressorTest.java
@@ -14,7 +14,6 @@ public class CompressorTest {
 
     @Test
     public void shouldCompressSucceeds() throws Exception {
-        System.out.println("Start compress test");
 
         Settings.inputZipDir = "/Users/kumaryadav/Desktop/JustExampleFolder";
         Settings.outputZipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out";
@@ -22,14 +21,12 @@ public class CompressorTest {
 
         Compressor compressor = new CompressorImpl();
         Assert.assertTrue(compressor.compress());
-        System.out.println("Done");
     }
 
     @Test
     public void shouldCompressFailsForSourceDirDoesNotExist() throws IOException {
 
         Settings.inputZipDir = "/Users/kumaryadav/Desktop/" + Math.random() + Math.random() + File.separator;
-        System.out.println(Settings.inputZipDir);
         Settings.outputZipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out";
         Settings.maxSize = 1;
 
--- src/test/java/archive/DecompressorTest.java
+++ src/test/java/archive/DecompressorTest.java
@@ -13,21 +13,18 @@ public class DecompressorTest {
 
     @Test
     public void shouldDecompress() throws Exception {
-        System.out.println("Start decompress test");
 
         Settings.inputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out";
         Settings.outputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/in_1";
 
         Decompressor decompressor = new DecompressorImpl();
         Assert.assertTrue(decompressor.decompress());
-        System.out.println("Done");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldCompressFailsForSourceDirDoesNotExist() throws IOException {
 
         Settings.inputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out" + Math.random() + Math.random() + File.separator;
-        System.out.println(Settings.inputUnzipDir);
         Settings.outputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/in";
 
         Decompressor decompressor = new DecompressorImpl();
@@ -39,7 +36,6 @@ public class DecompressorTest {
     public void shouldCompressFailsForSourceIsNotADirectory() throws IOException {
 
         Settings.inputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/out/staff.csv";
-        System.out.println(Settings.inputUnzipDir);
         Settings.outputUnzipDir = "/Users/kumaryadav/Desktop/JustExampleFolder/in";
 
         Decompressor decompressor = new DecompressorImpl();
